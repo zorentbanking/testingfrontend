8fa72d346ed964cd0e8638ce7be39d5c29b85c0f
Sappidi Hemanth <sappidi.hemanth@mphasis.com>
2026-05-20 09:59:18 +0530

    footer update

---
+++ src/app/footer/footer.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-footer',
+
+  templateUrl: './footer.html',
+
+  styleUrls: ['./footer.css']
+
+})
+
+export class FooterComponent {
+
+}
